e7ce3ed628a3134a30c9829c1a0c8e3761e8c48d
funsho <fob1493@gmail.com>
2017-07-04 22:30:55 +0100

    updated es5 version

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,9 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+    value: true
+});
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
@@ -32,7 +36,8 @@ var Human = function () {
         key: 'sleep',
         value: function sleep() {
 
-            console.log('zzz...');
+            console.log('Zzz...');
+            return 'Zzz...';
         }
     }, {
         key: 'calculateAge',
@@ -41,24 +46,28 @@ var Human = function () {
             var year = +new Date().getFullYear();
             var age = year - this.yearOfBirth;
             console.log('I am ' + age + ' years old');
+            return age;
         }
     }, {
         key: 'eat',
         value: function eat() {
 
             console.log('This food is really delicious!');
+            return 'This food is really delicious!';
         }
     }, {
         key: 'talk',
         value: function talk() {
 
             console.log('What do you want us to talk about?');
+            return 'What do you want us to talk about?';
         }
     }, {
         key: 'walk',
         value: function walk() {
 
-            console.log('Time to strech my legs');
+            console.log('Time to stretch my legs');
+            return 'Time to stretch my legs';
         }
     }]);
 
@@ -90,9 +99,11 @@ var Doctor = function (_Human) {
             if (this.available) {
 
                 console.log('Take these drugs, they will make you feel better by tomorrow');
+                return 'Take these drugs, they will make you feel better by tomorrow';
             } else {
 
                 console.log('Not available to attend to you right now');
+                return 'Not available to attend to you right now';
             }
         }
 
@@ -105,9 +116,11 @@ var Doctor = function (_Human) {
             if (!this.available) {
 
                 console.log('This food is really delicious!');
+                return 'This food is really delicious!';
             } else {
 
                 console.log('No time to eat, gotta attend to my patients first');
+                return 'No time to eat, gotta attend to my patients first';
             }
         }
     }, {
@@ -117,10 +130,21 @@ var Doctor = function (_Human) {
             if (this.available) {
 
                 console.log('Don\'t be afraid, it doesn\'t hurt');
+                return 'Don\'t be afraid, it doesn\'t hurt';
             } else {
 
                 console.log('Not available to give any injection');
+                return 'Not available to give any injection';
             }
+        }
+    }, {
+        key: 'yearsOfPractice',
+        value: function yearsOfPractice() {
+
+            var year = +new Date().getFullYear();
+            var docYears = year - this.practice;
+            console.log('I\'ve been practising as a Doctor for ' + docYears + ' years');
+            return docYears;
         }
     }]);
 
@@ -143,7 +167,7 @@ var Driver = function (_Human2) {
         return _this2;
     }
 
-    //overwrites inheritted eat function
+    //overwrites inheritted eat method
 
 
     _createClass(Driver, [{
@@ -151,6 +175,7 @@ var Driver = function (_Human2) {
         value: function eat() {
 
             console.log('Abeg give me more of this delicious meal');
+            return 'Abeg give me more of this delicious meal';
         }
     }, {
         key: 'getLicense',
@@ -159,10 +184,12 @@ var Driver = function (_Human2) {
             if (this.isLicensed) {
 
                 console.log('I already have a valid license to drive');
+                return 'I already have a valid license to drive';
             } else {
 
                 this.isLicensed = true;
                 console.log('Now I have a valid license to drive');
+                return this.isLicensed;
             }
         }
     }]);
@@ -193,48 +220,29 @@ var Programmer = function (_Human3) {
         key: 'sleep',
         value: function sleep() {
 
-            console.log(' No way, sleep is for the weak');
+            console.log('No way, sleep is for the weak');
+            return 'No way, sleep is for the weak';
         }
     }, {
         key: 'code',
         value: function code() {
 
-            console.log('code...code...code');
+            if (this.canCode) {
+
+                console.log('code...code...code');
+                return 'code...code...code';
+            } else {
+
+                console.log("Learning to code...");
+                return 'Learning to code...';
+            }
         }
     }]);
 
     return Programmer;
 }(Human);
 
-var mike = new Human(1994, 'black', 'male');
-mike.greet();
-console.log(mike.legs);
-console.log(mike.sex);
-mike.calculateAge();
-mike.talk();
-mike.eat();
-mike.walk();
-
-var susan = new Doctor(1997, 'light', 'female', false, 2000);
-
-susan.greet('Hi, I am Susan');
-console.log(susan.legs);
-susan.eat();
-susan.giveInjection();
-
-var hammed = new Driver(1981, 'black', 'male', false);
-console.log(hammed.isLicensed);
-hammed.greet();
-
-var funsho = new Programmer();
-funsho.code();
-funsho.greet();
-funsho.sleep();
-console.log(funsho.hands);
-
-exports = {
-
-    Human: Human,
-    Driver: Driver
-
-};
+exports.Human = Human;
+exports.Driver = Driver;
+exports.Doctor = Doctor;
+exports.Programmer = Programmer;
